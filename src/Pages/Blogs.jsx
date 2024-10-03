c455867d08be180eb9138945e5449eaf84a2f77e
YousufMOhammadi <yousufmohammadi3222@gmail.com>
2024-10-03 06:47:07 +0330

    blogs page responsived

--- src/Pages/Blogs.jsx
+++ src/Pages/Blogs.jsx
@@ -8,9 +8,7 @@ function Blogs() {
     <main className="h-auto w-full">
       <Header />
       <Cocumbar title="Blogs" />
-      <div className="mt-56 pb-10 pt-32">
-        <Blog />
-      </div>
+      <Blog />
       <Footer />
     </main>
   );
