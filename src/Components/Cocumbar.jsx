c0e9147b5bc3c50fcacdc2c58187668e97a4831b
YousufMOhammadi <yousufmohammadi3222@gmail.com>
2024-10-03 07:09:51 +0330

    cocumbar component responsived

--- src/Components/Cocumbar.jsx
+++ src/Components/Cocumbar.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 function Cocumbar(props) {
   return (
     <section className="cocumbar relative">
-      <span className="absolute left-28 top-1/3 font-poppins text-4xl font-bold text-slate-900">
+      <span className="absolute left-28 top-1/3 font-poppins text-2xl font-medium text-slate-900 md:text-4xl md:font-bold">
         {props.title}
       </span>
     </section>
