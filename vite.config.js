3b1479e25ef155e259480ea07fabf3755fc16829
YousufMOhammadi <yousufmohammadi3222@gmail.com>
2024-10-03 07:28:43 +0330

    build config added

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,16 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	server: {
-		port: 3000,
-	},
-	preview: {
-		port: 8080,
-	},
+  plugins: [react()],
+  build: {
+    outDir: 'dist', // This should point to the correct directory
+  },
+  server: {
+    port: 3000,
+  },
+  preview: {
+    port: 8080,
+  },
 });
